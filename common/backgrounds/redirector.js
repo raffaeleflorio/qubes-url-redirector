22a78de378ac1c415522ec6a5c95a517776b7d3c
Raffaele Florio <raffaeleflorio@protonmail.com>
2017-11-02 11:36:21 +0100

    Fixed #2

--- common/backgrounds/redirector.js
+++ common/backgrounds/redirector.js
@@ -1,11 +1,29 @@
 const redirector = () => {
     let processedRequest = [];
+    /* used to fix #2 */
+    let blockedTabs = {};
 
     return ({
 	route: (details) => {
 	    /* chrome fix */
 	    if (!/^https?:\/\//.test(details.url))
 		return {cancel: false};
+
+	    let tab = blockedTabs[details.tabId];
+	    if (tab) {
+		url = tab.redirectUrl;
+		if (url !== null && url !== details.url) {
+		    browser.tabs.update(details.tabId, {url}); /* if something goes wrong the next request is blocked */
+		    delete blockedTabs[details.tabId];
+		    return {redirectUrl: url};
+		} else if (url !== null && url === details.url) { /* if the URL is typed */
+		    browser.tabs.remove(details.tabId);
+		    delete blockedTabs[details.tabId];
+		    return {cancel: true};
+		} else { /* if a redirectUrl is not yet setted */
+		    return {cancel: true};
+		}
+	    }
 
 	    if (processedRequest.indexOf(details.requestId) !== -1)
 		return {cancel: false};
@@ -17,14 +35,14 @@ const redirector = () => {
 	    if (!openHere) {
 		const vmname = default_action === qur.settings.ACTION.DVM ? "$dispvm" : qur.settings.getDefaultVm();
 
-		/* TODO: fix for Chrome */
-		/* Currently immediate closing, in every browser, because issue #2 */
-		if (chrome)
-		    browser.tabs.remove(details.tabId);
-		else
-		    browser.tabs.get(details.tabId)
+		if (browser.isPolyfilled === true)
+		    blockedTabs[details.tabId] = {"redirectUrl": null};
+
+		browser.tabs.get(details.tabId)
 		    .then((requestTab) => {
-			if (requestTab.url === "about:blank" || requestTab.url === "about:newtab" || requestTab.url === details.url)
+			if (browser.isPolyfilled === true) /* Chrome case */
+			    blockedTabs[requestTab.id].redirectUrl = requestTab.url;
+			else if (requestTab.url === "about:blank" || requestTab.url === "about:newtab") /* Firefox case */
 			    browser.tabs.remove(details.tabId);
 		    });
 		
