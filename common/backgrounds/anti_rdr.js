72133746e9e05add18adad562cae99ce89c26710
Raffaele Florio <raffaeleflorio@protonmail.com>
2018-02-02 17:15:07 +0100

    added license

--- common/backgrounds/anti_rdr.js
+++ common/backgrounds/anti_rdr.js
@@ -1,3 +1,22 @@
+/*
+ * Copyright (C) 2017 Raffaele Florio <raffaeleflorio@protonmail.com>
+ *
+ * This file is part of qubes-url-redirector.
+ *
+ * qubes-url-redirector is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * qubes-url-redirector is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with qubes-url-redirector.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
 const anti_rdr = () => {
     const buildAnti = (regex, parameter) => {
 	return {"r": regex, "p": parameter};
