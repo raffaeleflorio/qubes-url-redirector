86fd3030fe59508391927667819203f57d633e6e
Raffaele Florio <raffaeleflorio@protonmail.com>
2018-03-09 18:35:55 +0100

    GET_WHITELIST implemented

---
+++ common/js/backend/messaging/whitelistML.js
@@ -0,0 +1,36 @@
+/*
+ * Copyright (C) 2017,2018 Raffaele Florio <raffaeleflorio@protonmail.com>
+ *
+ * This file is part of qubes-url-redirector.
+ *
+ * qubes-url-redirector is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * qubes-url-redirector is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with qubes-url-redirector.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+QUR.messaging.addListener({
+    msg: QUR.messaging.MSG.GET_WHITELIST,
+    handler: function (details) {
+	"use strict";
+
+	const {sendResponse} = details;
+	const ret = [];
+	QUR.whitelist.forEach(function (entry) {
+	    ret.push({
+		type: entry.getType(),
+		simpleString: entry.toString(),
+		detailedString: entry.toString(true)
+	    });
+	});
+	sendResponse({response: ret});
+    }
+});
