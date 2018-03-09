cde45a8cc3338586bde530483d79e2b97f954235
Raffaele Florio <raffaeleflorio@protonmail.com>
2018-03-09 18:20:44 +0100

    implementend persistence in whitelist.js

--- common/js/backend/core/whitelist.js
+++ common/js/backend/core/whitelist.js
@@ -17,47 +17,85 @@
  * along with qubes-url-redirector.  If not, see <http://www.gnu.org/licenses/>.
 */
 
-QUR.whitelist = (function () {
+(function () {
     "use strict";
 
     let _whitelist = [];
 
     const findIndex = (entry) => _whitelist.findIndex((x) => x.toString(true) === entry.toString(true));
 
+    const getCloned = () => _whitelist.slice(0);
+    const persist = (whitelist) => browser.storage.local.set({"whitelist": JSON.parse(JSON.stringify(whitelist))});
+
     const that = QUR.whitelist_entries;
-    return Object.freeze({
+    const publicWhitelist = Object.freeze({
 	toString: () => _whitelist.toString(),
 	toJSON: () => _whitelist.slice(0),
 	test: (value) => _whitelist.some((x) => x.test(value)),
 	add (entry) {
 	    if (findIndex(entry) >= 0 || !that.isValidEntry(entry)) {
-		return false;
+		return Promise.resolve(false);
 	    }
 
-	    _whitelist.push(entry);
-	    return true;
+	    const cloned = getCloned();
+	    cloned.push(entry);
+	    return persist(cloned).then(function () {
+		_whitelist.push(entry);
+		return Promise.resolve(true);
+	    });
 	},
 	rm (entry) {
 	    const i = findIndex(entry);
 	    if (i === -1) {
-		return false;
+		return Promise.resolve(false);
 	    }
 	    
-	    _whitelist.splice(i, 1);
-	    return true;
+	    const cloned = getCloned();
+	    cloned.splice(i, 1);
+	    return persist(cloned).then(function () {
+		_whitelist.splice(i, 1);
+		return Promise.resolve(true);
+	    });
 	},
 	replace (oldEntry, newEntry) {
 	    const i = findIndex(oldEntry);
 	    const j = findIndex(newEntry);
 	    if (i === -1 || j >= 0 || !that.isValidEntry(newEntry)) {
-		return false;
+		return Promise.resolve(false);
 	    }
 
-	    _whitelist[i] = newEntry;
-	    return true;
+	    const cloned = getCloned();
+	    cloned[i] = newEntry;
+	    return persist(cloned).then(function () {
+		_whitelist[i] = newEntry;
+		return Promise.resolve(true);
+	    });
 	},
 	clear () {
 	    _whitelist = [];
 	}
     });
+
+    browser.storage.local.get("whitelist")
+	.then(function (result) {
+	    const whitelist = result.whitelist;
+	    if (Array.isArray(whitelist)) {
+		return whitelist.map(that.makeEntry).filter((e) => e !== null);
+	    }
+
+	    return _whitelist;
+	})
+	.then((whitelist) => whitelist.forEach((e) => _whitelist.push(e)))
+	.then(() => persist(_whitelist))
+	.then(function () {
+	    QUR.whitelist = publicWhitelist;
+	    console.log("[INFO] QUR.whitelist initialized");
+	})
+	.catch(function (error) {
+	    const msg = [
+		"Error during QUR.whitelist initialization: " + error.toString(),
+		"The extension will not work!"
+	    ].join("\n");
+	    console.error(msg);
+	});
 }());
