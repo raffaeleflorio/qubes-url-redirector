0a81672f5ffa22880e20ada04df736ce99af61e2
Raffaele Florio <raffaeleflorio@protonmail.com>
2018-03-15 13:27:23 +0100

    frontend: Reset Button added to the whitelist form, and whitelist iife

--- common/js/frontend/settings/whitelist.js
+++ common/js/frontend/settings/whitelist.js
@@ -17,83 +17,12 @@
  * along with qubes-url-redirector.  If not, see <http://www.gnu.org/licenses/>.
  */
 
-OPTIONS.whitelist = Object.freeze({
-    submitHandler (ev) {
-	"use strict";
-
-	const form = ev.target;
-
-	const entrySpec = {
-	    type: Number(form.type.value),
-	    spec: null
-	};
-
-	if (entrySpec.type === OPTIONS.whitelist_entries.ENTRY_TYPE.DOMAIN) {
-	    entrySpec.spec = {};
-	    entrySpec.spec.domain = form.spec.value;
-	    entrySpec.spec.subdomain =  form.subdomain.checked;
-	} else {
-	    entrySpec.spec = form.spec.value;
-	}
-
-	const MSG = OPTIONS.messaging.MSG;
-	const sendMessage = OPTIONS.messaging.sendMessage;
-	sendMessage({msg: MSG.ADD_TO_WHITELIST, options: entrySpec})
-	    .then(function (result) {
-		if (result) {
-		    alert("Entry added successfully");
-		    OPTIONS.whitelist.addEntry(entrySpec);
-		} else {
-		    alert("Unable to add entry!");
-		}
-	    })
-	    .catch((error) => OPTIONS.fatal(error));
-
-	ev.preventDefault();
-    },
-    render (entries) {
-	"use strict";
-
-	const that = OPTIONS.whitelist;
-	entries.forEach(that.addEntry);
-    },
-    addEntry (entrySpec) {
-	"use strict";
-
-	const table = document.getElementById("whitelist_entries");
-
-	const entry = OPTIONS.whitelist_entries.makeEntry(entrySpec);
-	const row = entry.getHTMLRow();
-
-	const MSG = OPTIONS.messaging.MSG;
-	const sendMessage = OPTIONS.messaging.sendMessage;
-
-	const rmBtn = row.childNodes[4];
-	rmBtn.addEventListener("click", function (ev) {
-	    sendMessage({msg: MSG.RM_FROM_WHITELIST, options: entrySpec})
-		.then(function (result) {
-		    if (result) {
-			table.removeChild(row);
-			alert("Entry removed successfully!");
-		    } else {
-			alert("Unable to remove entry!");
-		    }
-		})
-		.catch((error) => OPTIONS.fatal(error));
-	});
-
-	row.className = "entry";
-	table.appendChild(row);
-    }
-});
-
-(function () {
+OPTIONS.whitelist = (function () {
     "use strict";
 
-    /*
-      called when the selected entry change, in the "type to add " form
-    */
-    function changeTypeDetails (ev) {
+    /* set the handler that will be called when the type radio change */
+    const entryTypeRadios = Array.from(document.getElementById("whitelist").type);
+    entryTypeRadios.forEach((e) => e.addEventListener("change", function (ev) {
 	const checkedType = Number(ev.target.value);
 
 	const typeInfo = [];
@@ -109,9 +38,99 @@ OPTIONS.whitelist = Object.freeze({
 	    "Domain: www."
 	];
 	document.getElementById("type_label").firstChild.textContent = typeLabel[checkedType];
-    }
+    }));
 
-    const entryType = Array.from(document.getElementById("whitelist").type);
-    entryType.forEach((e) => e.addEventListener("change", changeTypeDetails));
+    /* Reset button handler */
+    document.getElementById("whitelistReset").addEventListener("click", function (ev) {
+	const form = document.getElementById("whitelist");
+	form.reset();
+    });
+
+    return Object.freeze({
+	submitHandler (ev) {
+	    "use strict";
+
+	    const form = ev.target;
+
+	    const entrySpec = {
+		type: Number(form.type.value),
+		spec: null
+	    };
+
+	    if (entrySpec.type === OPTIONS.whitelist_entries.ENTRY_TYPE.DOMAIN) {
+		entrySpec.spec = {};
+		entrySpec.spec.domain = form.spec.value;
+		entrySpec.spec.subdomain =  form.subdomain.checked;
+	    } else {
+		entrySpec.spec = form.spec.value;
+	    }
+
+	    const MSG = OPTIONS.messaging.MSG;
+	    const sendMessage = OPTIONS.messaging.sendMessage;
+	    sendMessage({msg: MSG.ADD_TO_WHITELIST, options: entrySpec})
+		.then(function (result) {
+		    if (result) {
+			alert("Entry added successfully");
+			OPTIONS.whitelist.addEntry(entrySpec);
+		    } else {
+			alert("Unable to add entry!");
+		    }
+		})
+		.catch((error) => OPTIONS.fatal(error));
+
+	    ev.preventDefault();
+	},
+	render (entries) {
+	    "use strict";
+
+	    const that = OPTIONS.whitelist;
+	    entries.forEach(that.addEntry);
+	},
+	addEntry (entrySpec) {
+	    "use strict";
+
+	    const table = document.getElementById("whitelist_entries");
+
+	    const entry = OPTIONS.whitelist_entries.makeEntry(entrySpec);
+	    const row = entry.getHTMLRow();
+
+	    const MSG = OPTIONS.messaging.MSG;
+	    const sendMessage = OPTIONS.messaging.sendMessage;
+
+	    const modifyBtn = row.childNodes[3];
+	    /* Modify Button handler */
+	    modifyBtn.addEventListener("click", function (ev) {
+		const form = document.getElementById("whitelist");
+		form.type.value = entrySpec.type;
+		if (entrySpec.type === OPTIONS.whitelist_entries.ENTRY_TYPE.DOMAIN) {
+		    form.spec.value = entrySpec.spec.domain;
+		} else {
+		    form.spec.value = entrySpec.spec;
+		}
+		form.subdomain.checked = entrySpec.spec.subdomain || false;
+
+
+		const whitelistSubmit = document.getElementById("whitelistSubmit");
+		whitelistSubmit.textContent = "Modify";
+	    });
+
+	    const rmBtn = row.childNodes[4];
+	    /* Remove Button handler */
+	    rmBtn.addEventListener("click", function (ev) {
+		sendMessage({msg: MSG.RM_FROM_WHITELIST, options: entrySpec})
+		    .then(function (result) {
+			if (result) {
+			    table.removeChild(row);
+			    alert("Entry removed successfully!");
+			} else {
+			    alert("Unable to remove entry!");
+			}
+		    })
+		    .catch((error) => OPTIONS.fatal(error));
+	    });
+
+	    row.className = "entry";
+	    table.appendChild(row);
+	}
+    });
 }());
-
