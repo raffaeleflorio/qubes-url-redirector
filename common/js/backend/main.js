4d30635c6a5c4c8c03955cf33779298437e56b61
Raffaele Florio <raffaeleflorio@protonmail.com>
2018-08-27 13:24:18 +0200

    Access the settings page through browserAction.

--- common/js/backend/main.js
+++ common/js/backend/main.js
@@ -59,3 +59,7 @@ const QUR = (function () {
 browser.runtime.onInstalled.addListener(function () {
     browser.tabs.create({url: browser.runtime.getURL("/common/html/welcome.html")});
 });
+
+browser.browserAction.onClicked.addListener(function () {
+    browser.runtime.openOptionsPage();
+});
